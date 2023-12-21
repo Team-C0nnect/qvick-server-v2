e8a4080f3a2e382ef0a18050dd4fba919886ee3b
lgwk42 <luisgunkim@gmail.com>
2023-12-21 12:17:42 +0900

    :sparkles: [Feature] :: 서버 yml 파일 세팅

---
+++ src/main/java/com/project/qvick/QvickApplication.java
@@ -0,0 +1,13 @@
+package com.project.qvick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QvickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QvickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/qvick/QvickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.qvick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QvickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
