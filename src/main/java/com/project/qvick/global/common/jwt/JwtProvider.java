e489afbbd08077b3ffef07327f08d78a0d9ba587
lgwk42 <luisgunkim@gmail.com>
2024-01-10 14:14:05 +0900

    :sparkles: [Feature] :: jwt 개발

---
+++ src/main/java/com/project/qvick/global/common/jwt/JwtProvider.java
@@ -0,0 +1,99 @@
+package com.project.qvick.global.common.jwt;
+
+import com.project.checkin.domain.user.domain.enums.UserRole;
+import com.project.qvick.global.common.jwt.exception.TokenTypeException;
+import com.project.checkin.domain.user.domain.repository.UserRepository;
+import com.project.checkin.domain.user.exception.UserNotFoundException;
+import com.project.checkin.domain.user.mapper.UserMapper;
+import com.project.checkin.domain.user.dto.User;
+import com.project.qvick.global.common.jwt.config.JwtProperties;
+import com.project.qvick.global.common.jwt.enums.JwtType;
+import com.project.checkin.global.security.auth.principal.CustomUserDetails;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+
+    private final JwtProperties jwtProperties;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public Jws<Claims> getClaims(final String token) {
+        try {
+            return Jwts.parser().setSigningKey(jwtProperties.getSecretKey()).parseClaimsJws(token);
+        } catch (ExpiredJwtException e) {
+            throw new IllegalArgumentException("만료된 토큰");
+        } catch (UnsupportedJwtException e) {
+            throw new IllegalArgumentException("지원되지 않는 토큰");
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("잘못된 토큰");
+        }
+    }
+
+    public String generateAccessToken(final String email, final UserRole userRole) {
+        return Jwts.builder()
+                .setHeaderParam(Header.JWT_TYPE, JwtType.ACCESS)
+                .setSubject(email)
+                .claim("authority", userRole)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.getExpiration()))
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
+
+    public String generateRefreshToken(final String email, final UserRole userRole) {
+        return Jwts.builder()
+                .setHeaderParam(Header.JWT_TYPE, JwtType.REFRESH)
+                .setSubject(email)
+                .claim("authority", userRole)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.getRefreshExpiration()))
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
+
+    public Authentication getAuthentication(final String token) {
+        final Jws<Claims> claims = getClaims(token);
+
+        if (isWrongType(claims, JwtType.ACCESS)) {
+            throw TokenTypeException.EXCEPTION;
+        }
+
+        User user = userRepository.findByEmail(claims.getBody().getSubject()).map(userMapper::toUser).orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+
+        final CustomUserDetails details = new CustomUserDetails(user);
+
+        return new UsernamePasswordAuthenticationToken(details, null, details.getAuthorities());
+    }
+
+    public String extractTokenFromRequest(HttpServletRequest request) {
+        return extractToken(request.getHeader(HttpHeaders.AUTHORIZATION));
+    }
+
+    public String extractToken(final String token) {
+        if (StringUtils.hasText(token) && token.startsWith("Bearer ")) {
+            return token.substring(7);
+        }
+        return token;
+    }
+
+    public boolean isWrongType(final Jws<Claims> claims, final JwtType jwtType) {
+        return !(claims.getHeader().get(Header.JWT_TYPE).equals(jwtType.toString()));
+    }
+}
---
+++ src/main/java/com/project/qvick/global/common/jwt/exception/TokenTypeException.java
@@ -0,0 +1,13 @@
+package com.project.qvick.global.common.jwt.exception;
+
+import com.project.qvick.global.common.jwt.exception.error.JwtTokenError;
+import com.project.checkin.global.exception.BusinessException;
+
+public class TokenTypeException extends BusinessException {
+
+    public static final TokenTypeException EXCEPTION = new TokenTypeException();
+
+    private TokenTypeException() {
+        super(JwtTokenError.JWT_TOKEN_ERROR);
+    }
+}
