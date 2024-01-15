4f3f86814ff7ad62d1497fc1ec3a188e5e649100
lgwk42 <luisgunkim@gmail.com>
2024-01-15 16:19:56 +0900

    :sparkles: [Feature] :: Wifi domain, presentation 레이어 개발

---
+++ src/main/java/com/project/qvick/domain/wifi/presentation/WifiController.java
@@ -0,0 +1,4 @@
+package com.project.qvick.domain.wifi.presentation;
+
+public class WifiController {
+}
---
+++ src/main/java/com/project/qvick/domain/wifi/presentation/dto/request/WifiRequest.java
@@ -0,0 +1,12 @@
+package com.project.qvick.domain.wifi.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WifiRequest {
+
+    private String ssid;
+
+}
