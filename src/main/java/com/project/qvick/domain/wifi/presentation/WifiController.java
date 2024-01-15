c179465d3de883caecd66f899b38583845352ffe
lgwk42 <luisgunkim@gmail.com>
2024-01-15 16:21:54 +0900

    :sparkles: [Feature] :: service 레이어 개발

--- src/main/java/com/project/qvick/domain/wifi/presentation/WifiController.java
+++ src/main/java/com/project/qvick/domain/wifi/presentation/WifiController.java
@@ -1,4 +1,14 @@
 package com.project.qvick.domain.wifi.presentation;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/wifi")
 public class WifiController {
+
+
+
 }
---
+++ src/main/java/com/project/qvick/domain/wifi/service/WifiService.java
@@ -0,0 +1,4 @@
+package com.project.qvick.domain.wifi.service;
+
+public interface WifiService {
+}
