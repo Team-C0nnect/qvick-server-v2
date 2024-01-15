46faf62ca696e0cc24767ca04225d12163b8f2b7
lgwk42 <luisgunkim@gmail.com>
2024-01-15 13:33:44 +0900

    :sparkles: [Feature] :: 학번 조회 서비스 로직 개발

--- src/main/java/com/project/qvick/domain/student/service/StudentService.java
+++ src/main/java/com/project/qvick/domain/student/service/StudentService.java
@@ -1,4 +1,7 @@
 package com.project.qvick.domain.student.service;
 
+import com.project.qvick.domain.student.presentation.dto.request.StudentRequest;
+
 public interface StudentService {
+    void findStudent(StudentRequest studentRequest);
 }
--- src/main/java/com/project/qvick/domain/student/service/StudentServiceImpl.java
+++ src/main/java/com/project/qvick/domain/student/service/StudentServiceImpl.java
@@ -1,7 +1,19 @@
 package com.project.qvick.domain.student.service;
 
+import com.project.qvick.domain.student.domain.repository.StudentRepository;
+import com.project.qvick.domain.student.presentation.dto.request.StudentRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class StudentServiceImpl implements StudentService{
+
+    private final StudentRepository studentRepository;
+
+    @Override
+    public void findStudent(StudentRequest studentRequest){
+        studentRepository.findByStdId(studentRequest.getStdId());
+    }
+
 }
