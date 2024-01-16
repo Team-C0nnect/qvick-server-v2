da875d63170e1ff18039755f40fb9131b2d5294e
lgwk42 <luisgunkim@gmail.com>
2024-01-16 10:16:27 +0900

    :sparkles: [Feature] :: wifi 조회 및 등록 API 개발

--- src/main/java/com/project/qvick/domain/wifi/presentation/WifiController.java
+++ src/main/java/com/project/qvick/domain/wifi/presentation/WifiController.java
@@ -1,14 +1,34 @@
 package com.project.qvick.domain.wifi.presentation;
 
+import com.project.qvick.domain.wifi.presentation.dto.request.WifiRequest;
+import com.project.qvick.domain.wifi.service.WifiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "와이파이", description = "와이파이")
 @RequestMapping("/wifi")
 public class WifiController {
 
+    private final WifiService wifiService;
 
+    @Operation(summary = "와이파이 등록", description = "와이파이를 등록합니다")
+    @PostMapping("/register")
+    public void wifiRegister(WifiRequest wifiRequest){
+        wifiService.wifiRegister(wifiRequest);
+    }
+
+    @Operation(summary = "와이파이 확인", description = "와이파이를 확인합니다")
+    @GetMapping("")
+    public ResponseEntity<Void> wifiCheck(WifiRequest wifiRequest){
+        return wifiService.wifiCheck(wifiRequest);
+    }
 
 }
