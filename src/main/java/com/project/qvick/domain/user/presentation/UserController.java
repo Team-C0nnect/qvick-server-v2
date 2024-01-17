ac21e4528f48593b67596ff023b631e22d9803f5
Sewook <sewook0303@dgsw.hs.kr>
2024-01-17 15:03:52 +0900

    :memo: [Feature] :: 쿼리 서비스 컨트롤러 개발

--- src/main/java/com/project/qvick/domain/user/presentation/UserController.java
+++ src/main/java/com/project/qvick/domain/user/presentation/UserController.java
@@ -1,5 +1,7 @@
 package com.project.qvick.domain.user.presentation;
 
+import com.project.qvick.domain.user.presentation.dto.User;
+import com.project.qvick.domain.user.presentation.dto.request.UserApprovalPageRequest;
 import com.project.qvick.domain.user.presentation.dto.request.UserSignUpRequest;
 import com.project.qvick.domain.user.service.UserService;
 import com.project.qvick.domain.user.service.querydsl.UserQueryService;
@@ -7,12 +9,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,6 +40,12 @@ public class UserController {
     @PutMapping("/reject")
     public void userReject(@Validated @RequestBody UserSignUpRequest request){
         service.rejectSignUp(request);
+    }
+
+    @Operation(summary = "유저 조회", description = "승인 대기 유저를 조회합니다.")
+    @GetMapping("")
+    public ResponseEntity<List<User>> findWaitingUsers(UserApprovalPageRequest request){
+        return ResponseEntity.ok(queryService.findWaitingUsers(request));
     }
 
 }
