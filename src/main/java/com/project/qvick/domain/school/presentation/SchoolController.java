5c8095a681ffa334c91bf82555003140aa3471b8
lgwk42 <luisgunkim@gmail.com>
2024-01-17 13:57:21 +0900

    :sparkless: [Feature] :: 학교 presention 레이어 구축

---
+++ src/main/java/com/project/qvick/domain/school/presentation/SchoolController.java
@@ -0,0 +1,14 @@
+package com.project.qvick.domain.school.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/school")
+@RequiredArgsConstructor
+public class SchoolController {
+
+
+
+}
