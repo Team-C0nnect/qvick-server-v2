ecf4e121939fbb9c650ca247d2fb2c7f40bbccb4
lgwk42 <luisgunkim@gmail.com>
2024-02-06 22:21:05 +0900

    :sparkles: [Fix] :: room repository 추가

--- src/main/java/com/project/qvick/domain/room/domain/RoomEntity.java
+++ src/main/java/com/project/qvick/domain/room/domain/RoomEntity.java
@@ -1,14 +1,11 @@
 package com.project.qvick.domain.room.domain;
 
-import com.fasterxml.jackson.databind.ser.Serializers;
 import com.project.qvick.global.entity.BaseTimeEntity;
-import io.micrometer.core.annotation.Counted;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import org.checkerframework.checker.units.qual.C;
 
 @Entity
 public class RoomEntity extends BaseTimeEntity {
