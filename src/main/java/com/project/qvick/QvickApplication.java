e0007b78286b2358d2657be94faeee7f00f45ffd
lgwk42 <luisgunkim@gmail.com>
2024-03-11 08:34:24 +0900

    :fire: [Fix] :: redis host 삭제

--- src/main/java/com/project/qvick/QvickApplication.java
+++ src/main/java/com/project/qvick/QvickApplication.java
@@ -2,14 +2,10 @@ package com.project.qvick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-@EnableFeignClients
 public class QvickApplication {
 
 	public static void main(String[] args) {
