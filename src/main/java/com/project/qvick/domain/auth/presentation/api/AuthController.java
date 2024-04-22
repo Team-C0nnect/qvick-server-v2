a09650d350acb67f2b3c0b25c6dbc6b92e3eaff1
lgwk42 <luisgunkim@gmail.com>
2024-04-22 18:09:32 +0900

    [Fix] :: signUp controller divide

--- src/main/java/com/project/qvick/domain/auth/presentation/api/AuthController.java
+++ src/main/java/com/project/qvick/domain/auth/presentation/api/AuthController.java
@@ -1,19 +1,18 @@
 package com.project.qvick.domain.auth.presentation.api;
 
+import com.project.qvick.domain.auth.presentation.dto.request.AdminSignUpRequest;
 import com.project.qvick.domain.auth.presentation.dto.request.AuthenticationRequest;
 import com.project.qvick.domain.auth.presentation.dto.request.RefreshTokenRequest;
 import com.project.qvick.domain.auth.presentation.dto.request.SignInRequest;
 import com.project.qvick.domain.auth.presentation.dto.request.SignUpRequest;
+import com.project.qvick.domain.auth.presentation.dto.request.TeacherSignUpRequest;
+import com.project.qvick.domain.auth.presentation.dto.request.UserSignUpRequest;
 import com.project.qvick.domain.auth.presentation.dto.response.JsonWebTokenResponse;
 import com.project.qvick.domain.auth.service.AuthService;
-import com.project.qvick.domain.user.presentation.dto.User;
-import com.project.qvick.global.infra.firebase.service.FirebaseNotificationService;
-import com.project.qvick.global.infra.firebase.service.FirebaseNotificationServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,17 +28,31 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @Operation(summary = "회원가입", description = "회원가입")
-    @PostMapping("/sign-up")
+    @Operation(summary = "유저 회원가입", description = "유저 회원가입")
+    @PostMapping("/sign-up/user")
     @ResponseStatus(HttpStatus.CREATED)
-    public void signUp(@Validated @RequestBody SignUpRequest signUpRequest){
-        authService.signUp(signUpRequest);
+    public void userSignUp(@Validated @RequestBody UserSignUpRequest signUpRequest){
+        authService.userSignUp(signUpRequest);
+    }
+
+    @Operation(summary = "선생님 회원가입", description = "선생님 회원가입")
+    @PostMapping("/sign-up/teacher")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void teacherSignUp(@Validated @RequestBody TeacherSignUpRequest signUpRequest){
+        authService.teacherSignUp(signUpRequest);
+    }
+
+    @Operation(summary = "관리자 회원가입", description = "관리자 회원가입")
+    @PostMapping("/sign-up/admin")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void adminSignUp(@Validated @RequestBody AdminSignUpRequest signUpRequest){
+        authService.adminSignUp(signUpRequest);
     }
 
     @Operation(summary = "로그인", description = "로그인")
     @PostMapping("/sign-in/user")
     public JsonWebTokenResponse signIn(@Validated @RequestBody SignInRequest signInRequest){
-        return authService.signIn(signInRequest);
+        return authService.userSignIn(signInRequest);
     }
 
     @Operation(summary = "관리자 로그인", description = "관리자 로그인")
