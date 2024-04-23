1cd2ede793010fc376f58620d0e9b36095883b39
lgwk42 <luisgunkim@gmail.com>
2024-04-23 13:55:10 +0900

    [Fix] :: check code service fix

--- src/main/java/com/project/qvick/domain/check/service/CheckCodeServiceImpl.java
+++ src/main/java/com/project/qvick/domain/check/service/CheckCodeServiceImpl.java
@@ -4,6 +4,7 @@ import com.project.qvick.domain.check.domain.CheckCodeEntity;
 import com.project.qvick.domain.check.domain.repository.CheckCodeRepository;
 import com.project.qvick.domain.check.mapper.CheckCodeMapper;
 import com.project.qvick.domain.check.presentation.dto.response.CheckCodeResponse;
+import com.project.qvick.global.common.repository.UserSecurity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -18,12 +19,15 @@ public class CheckCodeServiceImpl implements CheckCodeService {
 
     private final CheckCodeRepository checkCodeRepository;
     private final CheckCodeMapper checkCodeMapper;
+    private final UserSecurity userSecurity;
+
+    Long id = userSecurity.getUser().getId();
 
     @Override
     @Async
     public CompletableFuture<CheckCodeResponse> generate() {
-        checkCodeRepository.updateAllInvalidCheckCode(1L);
-        CheckCodeEntity checkCodeEntity = checkCodeRepository.save(checkCodeMapper.createCheckCodeEntity(1L));
+        checkCodeRepository.updateAllInvalidCheckCode(id);
+        CheckCodeEntity checkCodeEntity = checkCodeRepository.save(checkCodeMapper.createCheckCodeEntity(id));
         return CompletableFuture.completedFuture(CheckCodeResponse.builder().code(checkCodeEntity.getCode()).build());
     }
 
