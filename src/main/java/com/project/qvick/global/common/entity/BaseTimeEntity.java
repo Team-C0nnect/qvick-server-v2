fb08510111a157d623f5cd533dfd87804a3707c1
lgwk42 <luisgunkim@gmail.com>
2024-05-08 20:03:39 +0900

    [Fix] :: fix base time entity

--- src/main/java/com/project/qvick/global/common/entity/BaseTimeEntity.java
+++ src/main/java/com/project/qvick/global/common/entity/BaseTimeEntity.java
@@ -1,5 +1,6 @@
 package com.project.qvick.global.common.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AccessLevel;
@@ -20,9 +21,11 @@ import java.time.LocalDateTime;
 public abstract class BaseTimeEntity {
 
     @CreatedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     protected LocalDateTime createdDateTime;
 
     @LastModifiedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     protected LocalDateTime modifiedDateTime;
 
 }
