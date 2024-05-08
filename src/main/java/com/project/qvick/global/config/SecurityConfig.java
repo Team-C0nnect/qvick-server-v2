43b033379d17641d040f57dd86ca9a3919d8118c
lgwk42 <luisgunkim@gmail.com>
2024-05-08 23:19:27 +0900

    [Fix] :: security config fix

--- src/main/java/com/project/qvick/global/config/SecurityConfig.java
+++ src/main/java/com/project/qvick/global/config/SecurityConfig.java
@@ -42,15 +42,15 @@ public class SecurityConfig {
                                 .requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
                                 .requestMatchers("/terms/**").permitAll()
                                 .requestMatchers("/auth/**").permitAll()
-                                .requestMatchers("/attendance/code").permitAll()
+                                .requestMatchers(POST,"/attendance/code").permitAll()
 
                                 .requestMatchers("/outing-admin/**").hasAnyAuthority(ADMIN,TEACHER)
-                                .requestMatchers("/attendance/list").hasAnyAuthority(ADMIN,TEACHER)
+                                .requestMatchers(GET,"/attendance/list").hasAnyAuthority(ADMIN,TEACHER)
                                 .requestMatchers("/sleepover-admin/**").hasAnyAuthority(ADMIN,TEACHER)
 
                                 .requestMatchers(POST,"/attendance").hasAuthority(USER)
                                 .requestMatchers(POST,"/outing").hasAuthority(USER)
-                                .requestMatchers(POST,"sleepover").hasAuthority(USER)
+                                .requestMatchers(POST,"/sleepover").hasAuthority(USER)
                                 .requestMatchers(PATCH,"/user/stdId").hasAuthority(USER)
                                 .requestMatchers(PATCH,"/user/room").hasAuthority(USER)
 
