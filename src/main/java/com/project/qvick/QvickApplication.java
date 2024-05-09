1e3d1e1f5811de3e6a2a2c887fe3ce800163e048
lgwk42 <luisgunkim@gmail.com>
2024-05-09 11:39:29 +0900

    [Fix] :: application annotation position fix

--- src/main/java/com/project/qvick/QvickApplication.java
+++ src/main/java/com/project/qvick/QvickApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableAsync
 public class QvickApplication {
 
 	public static void main(String[] args) {
