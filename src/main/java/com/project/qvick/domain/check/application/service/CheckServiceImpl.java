bc0ddc7fddeeab3905b12eb137278eea5969e18b
lgwk42 <luisgunkim@gmail.com>
2024-05-23 23:53:02 +0900

    [Fix] :: attendance error fix

--- src/main/java/com/project/qvick/domain/check/application/service/CheckServiceImpl.java
+++ src/main/java/com/project/qvick/domain/check/application/service/CheckServiceImpl.java
@@ -43,7 +43,6 @@ public class CheckServiceImpl implements CheckService {
         } else {
             throw CheckCodeExpirationException.EXCEPTION;
         }
-        throw CheckCodeError.EXCEPTION;
     }
 
     @Override
