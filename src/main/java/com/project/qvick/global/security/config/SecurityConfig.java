c21a2aab1fe6eb3e67c060a4f84e56ca0100ee45
lgwk42 <luisgunkim@gmail.com>
2024-06-04 16:04:29 +0900

    [Feat] :: SecurityConfiguration annotation add

--- src/main/java/com/project/qvick/global/security/config/SecurityConfig.java
+++ src/main/java/com/project/qvick/global/security/config/SecurityConfig.java
@@ -1,13 +1,12 @@
 package com.project.qvick.global.security.config;
 
+import com.project.qvick.global.annotation.SecurityConfiguration;
 import com.project.qvick.global.security.jwt.filter.JwtAuthenticationFilter;
 import com.project.qvick.global.security.jwt.filter.JwtExceptionFilter;
 import com.project.qvick.global.security.jwt.handler.JwtAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,10 +14,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-import static org.springframework.http.HttpMethod.*;
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.PATCH;
+import static org.springframework.http.HttpMethod.POST;
 
-@Configuration
-@EnableWebSecurity
+@SecurityConfiguration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
