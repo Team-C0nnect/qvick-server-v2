92acbe3df2cb8144686feb45a8983522e0f6abf5
lgwk42 <luisgunkim@gmail.com>
2024-06-04 15:55:01 +0900

    [Delete] :: unused annotation delete

--- src/main/java/com/project/qvick/global/security/jwt/JwtProvider.java
+++ src/main/java/com/project/qvick/global/security/jwt/JwtProvider.java
@@ -11,12 +11,10 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtProvider {
