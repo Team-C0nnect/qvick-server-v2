220fb4a73ad66451e938190448e6465a5b36e002
lgwk42 <luisgunkim@gmail.com>
2024-06-05 09:25:41 +0900

    [Fix] :: post get request fix

--- src/main/java/com/project/qvick/domain/post/client/api/PostController.java
+++ src/main/java/com/project/qvick/domain/post/client/api/PostController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,7 +40,7 @@ public class PostController {
 
     @GetMapping("")
     @Operation(summary = "공지사항 목록", description = "등록된 모든 공지사항 목록을 표시합니다")
-    public ResponseEntity<List<Post>> postList(PageRequest pageRequest){
+    public ResponseEntity<List<Post>> postList(@ModelAttribute PageRequest pageRequest){
         return ResponseEntity.ok().body(postQueryService.postList(pageRequest));
     }
 
