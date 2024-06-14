6066e62be22bcaf322094b44acbc17214d4c6af8
lgwk42 <luisgunkim@gmail.com>
2024-06-14 23:56:37 +0900

    fix :: outing domain response fix

--- src/main/java/com/project/qvick/domain/outing/client/api/OutingAdminController.java
+++ src/main/java/com/project/qvick/domain/outing/client/api/OutingAdminController.java
@@ -1,22 +1,21 @@
 package com.project.qvick.domain.outing.client.api;
 
+import com.project.qvick.domain.outing.application.query.OutingQueryService;
+import com.project.qvick.domain.outing.application.service.OutingService;
 import com.project.qvick.domain.outing.client.dto.Outing;
 import com.project.qvick.domain.outing.client.dto.request.OutingPageRequest;
 import com.project.qvick.domain.outing.client.dto.request.OutingSettingRequest;
-import com.project.qvick.domain.outing.application.service.OutingService;
-import com.project.qvick.domain.outing.application.query.OutingQueryService;
+import com.project.qvick.global.common.response.BaseResponse;
+import com.project.qvick.global.common.response.BaseResponseData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -31,23 +30,25 @@ public class OutingAdminController {
     private final OutingQueryService queryService;
 
     @GetMapping("/findStudents")
-    @Operation(summary = "외출 조회", description = "외출 명단을 조회합니다")
-    public ResponseEntity<List<Outing>> findStudents(@ModelAttribute OutingPageRequest request) {
-        return ResponseEntity.ok(queryService.findOutStudents(request));
+    @Operation(summary = "외출 명단 조회", description = "외출 명단을 조회합니다")
+    public BaseResponseData<List<Outing>> findStudents(@ModelAttribute OutingPageRequest request) {
+        return BaseResponseData.ok(
+                "외출 명단을 성공적으로 불러왔습니다.",
+                queryService.findOutStudents(request));
     }
 
     @PutMapping("/accept")
-    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "외출 승인", description = "외출을 승인합니다")
-    public void acceptSleepover(@RequestBody OutingSettingRequest request) {
+    public BaseResponse acceptSleepover(@RequestBody OutingSettingRequest request) {
         service.accept(request.getOutingId());
+        return BaseResponse.ok("외출이 승인되었습니다.");
     }
 
     @PutMapping("/refuse")
-    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "외출 거절", description = "외출을 거절합니다")
-    public void refuseSleepover(@Validated @RequestBody OutingSettingRequest request) {
+    public BaseResponse refuseSleepover(@Validated @RequestBody OutingSettingRequest request) {
         service.accept(request.getOutingId());
+        return BaseResponse.ok("외출이 거절되었습니다.");
     }
 
 }
--- src/main/java/com/project/qvick/domain/outing/client/api/OutingController.java
+++ src/main/java/com/project/qvick/domain/outing/client/api/OutingController.java
@@ -1,17 +1,16 @@
 package com.project.qvick.domain.outing.client.api;
 
-import com.project.qvick.domain.outing.client.dto.request.OutingRequest;
 import com.project.qvick.domain.outing.application.service.OutingService;
+import com.project.qvick.domain.outing.client.dto.request.OutingRequest;
+import com.project.qvick.global.common.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,10 +23,10 @@ public class OutingController {
     private final OutingService service;
 
     @PostMapping("")
-    @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "외출 신청", description = "외출을 신청합니다")
-    public void register(@Validated @RequestBody OutingRequest request){
+    public BaseResponse register(@Validated @RequestBody OutingRequest request){
         service.register(request.toOuting());
+        return BaseResponse.created("외출 신청 완료");
     }
 
 }
