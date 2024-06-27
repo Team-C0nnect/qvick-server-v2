e431c7c031293dc157cd1511c91cb9b7e66ad3cd
lgwk42 <luisgunkim@gmail.com>
2024-06-27 11:25:13 +0900

    feat :: token error / not support error add

--- src/main/java/com/project/qvick/global/security/jwt/exception/error/JwtTokenError.java
+++ src/main/java/com/project/qvick/global/security/jwt/exception/error/JwtTokenError.java
@@ -11,7 +11,9 @@ import org.springframework.http.HttpStatus;
 public enum JwtTokenError implements ErrorProperty {
 
     JWT_TOKEN_ERROR(HttpStatus.BAD_REQUEST, "잘못된 타입"),
-    JWT_EXPIRED(HttpStatus.GONE,"만료된 토큰");
+    JWT_EXPIRED(HttpStatus.GONE,"만료된 토큰"),
+    JWT_ERROR(HttpStatus.BAD_REQUEST,"잘못된 토큰"),
+    JWT_NOT_SUPPORT(HttpStatus.BAD_REQUEST,"지원하지 않는 토큰");
 
     private final HttpStatus status;
     private final String message;
