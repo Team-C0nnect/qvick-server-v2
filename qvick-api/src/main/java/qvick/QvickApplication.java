5aafdfa23b9c921ef48ef18c607c9b53b1782f81
lgwk42 <luisgunkim@gmail.com>
2024-06-27 23:26:43 +0900

    fix :: single -> multi-module settings

--- qvick-api/src/main/java/qvick/QvickApplication.java
+++ qvick-api/src/main/java/qvick/QvickApplication.java
@@ -1,19 +1,17 @@
-package com.project.qvick;
+package qvick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAsync
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableScheduling
 public class QvickApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QvickApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QvickApplication.class, args);
+    }
 
 }
