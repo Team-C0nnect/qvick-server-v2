e5a8707216c70825e773f7217712a088b9b003e5
lgwk42 <luisgunkim@gmail.com>
2024-06-28 12:33:43 +0900

    fix :: application run fix

--- src/main/java/com/project/qvick/QvickApplication.java
+++ src/main/java/com/project/qvick/QvickApplication.java
@@ -3,10 +3,8 @@ package com.project.qvick;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAsync
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableScheduling
