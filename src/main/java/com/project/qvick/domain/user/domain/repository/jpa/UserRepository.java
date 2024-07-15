c05f3f097fb0d6cff5e484e314c61a54a62b3fc5
lgwk42 <luisgunkim@gmail.com>
2024-07-15 23:41:14 +0900

    delete :: 사용하지 않는 import 삭제

--- src/main/java/com/project/qvick/domain/user/domain/repository/jpa/UserRepository.java
+++ src/main/java/com/project/qvick/domain/user/domain/repository/jpa/UserRepository.java
@@ -1,10 +1,8 @@
 package com.project.qvick.domain.user.domain.repository.jpa;
 
 import com.project.qvick.domain.user.domain.UserEntity;
-import jakarta.persistence.LockModeType;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
