a72074e47799f44515dc3fad54ab9bf67bcc7ad8
lgwk42 <luisgunkim@gmail.com>
2024-07-15 23:41:26 +0900

    feat :: 트랜잭션 추가

--- src/main/java/com/project/qvick/domain/post/domain/repository/jpa/PostRepository.java
+++ src/main/java/com/project/qvick/domain/post/domain/repository/jpa/PostRepository.java
@@ -1,10 +1,12 @@
 package com.project.qvick.domain.post.domain.repository.jpa;
 
 import com.project.qvick.domain.post.domain.PostEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
 
+    @Transactional(rollbackOn = Exception.class)
     void deleteById(Long id);
 
 }
