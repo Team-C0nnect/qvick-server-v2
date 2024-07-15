9822546df85229152f334cb4200179e2eabfc7cb
lgwk42 <luisgunkim@gmail.com>
2024-07-15 23:37:48 +0900

    delete :: 사용하지 않는 jpa 메서드 삭제

--- src/main/java/com/project/qvick/domain/user/domain/repository/jpa/UserRepository.java
+++ src/main/java/com/project/qvick/domain/user/domain/repository/jpa/UserRepository.java
@@ -1,8 +1,10 @@
 package com.project.qvick.domain.user.domain.repository.jpa;
 
 import com.project.qvick.domain.user.domain.UserEntity;
+import jakarta.persistence.LockModeType;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
@@ -12,8 +14,6 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
 
     Optional<UserEntity> findByStdId(String stdId);
-
-    UserEntity getByEmail(String email);
 
     @Transactional(rollbackOn = Exception.class)
     void deleteByEmail(String email);
